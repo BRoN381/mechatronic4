f37a9ac8158d766438d2cff7e29cf99b5e57785f
BRoN381 <91597656+BRoN381@users.noreply.github.com>
2023-11-02 17:57:34 +0800

    Update signDetect.py

--- signDetect.py
+++ signDetect.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
-cap=cv2.VideoCapture(0)
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 #==============CHANGE YOUR MASK HERE===============
-lower=np.array([  0,192,172])
-upper=np.array([  5,255,255])   
+lower=np.array([  0,146,115])
+upper=np.array([ 20,232,207])   
 
 def findSign(img):
     hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     mask=cv2.inRange(hsv,lower,upper)
     result=cv2.bitwise_and(img,img,mask=mask)
     result = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
-    contours, hierarchy = cv2.findContours(result, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  
+    _, contours, _ = cv2.findContours(result, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         cv2.drawContours(img, cnt, -1, (0, 255, 0), 3)
         area = cv2.contourArea(cnt)
@@ -22,16 +24,20 @@ def findSign(img):
             x, y, w, h = cv2.boundingRect(vertices)
             if corners == 8:  # find circle
                 cv2.putText(img, "circle", (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                return img
+                return img, '1'
     cv2.putText(img, "None", (10, 480-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    return img    
+    return img, '0'
 
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)  # Width
+cap.set(4, 480)  # Height
 
 while True:
     ret, frame = cap.read()  
     if ret:
-        imgContour=findSign(frame.copy())
+        imgContour, x =findSign(frame.copy())
         cv2.imshow("contour", imgContour)
+        ser.write(x.encode('utf-8'))
     else:
         break
     if cv2.waitKey(1)==ord("q"):
